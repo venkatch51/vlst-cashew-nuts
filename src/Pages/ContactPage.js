8f7be2e2d7865064d1ec52278916630c1c48d759
venkatch51 <venkatch8051@gmail.com>
2025-03-22 08:01:08 +0530

    Form is added

--- src/Pages/ContactPage.js
+++ src/Pages/ContactPage.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import { useForm } from "react-hook-form";
+import './ContactPage.css'
 
 const ContactPage = () => {
-  return (
-    <div className='container'>ContactPage</div>
-  )
-}
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+  console.log(errors);
 
-export default ContactPage
+  return (
+    <div className="form-section">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          type="text"
+          placeholder="Full Name"
+          {...register("Full Name", { required: true })}
+        />
+        <input
+          type="text"
+          placeholder="Email"
+          {...register("Email", { required: true, pattern: /^\S+@\S+$/i })}
+        />
+        <input
+          type="tel"
+          placeholder="Mobile number"
+          {...register("Mobile number", {
+            required: true,
+            minLength: 6,
+            maxLength: 12,
+          })}
+        />
+        <textarea {...register} type="textarea" placeholder="Leave Message" className="text-area-form"/>
+
+        <input type="submit" />
+      </form>
+    </div>
+  );
+};
+
+export default ContactPage;
